c6b920266db1e27110fdf92480be36ffe747b4e5
Perfect Makanju <damiperfect@gmail.com>
2022-06-06 00:05:50 +0100

    feat: generate sample query in graphiql
    This generats an example query based on the first defined entity in the
    schema argument to Checkpoint.

--- test/unit/graphql/controller.test.ts
+++ test/unit/graphql/controller.test.ts
@@ -56,4 +56,54 @@ type Vote {
       expect(mockMysql.queryAsync).toMatchSnapshot();
     });
   });
+
+  describe('generateSampleQuery', () => {
+    it('should return undefined when no entities are defined', () => {
+      const controller = new GqlEntityController('scalar Text');
+
+      expect(controller.generateSampleQuery()).toBeUndefined();
+    });
+
+    it.each([
+      {
+        case: 'first and only entity',
+        schema: `
+type Vote {
+  id: Int!
+  name: String
+  created_at: Int!
+}
+        `
+      },
+      {
+        case: 'nested objects',
+        // Checkpoint doesn't support relationship among entities,
+        // but this just tests to ensure the sample query works for it.
+        schema: `
+type Vote {
+  id: Int!
+  name: String
+  poster: Poster
+  created_at: Int!
+}
+
+type Poster {
+  id: Int!
+  name: String!
+  venue: Venue!
+}
+
+type Venue {
+  id: Int!
+  location: String!
+}
+        `
+      }
+    ])('should return correct query sample for $case', ({ schema }) => {
+      const controller = new GqlEntityController(schema);
+
+      expect(controller.generateSampleQuery()).not.toBeUndefined();
+      expect(controller.generateSampleQuery()).toMatchSnapshot();
+    });
+  });
 });
