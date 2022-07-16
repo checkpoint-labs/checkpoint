52ce18801e72c358480d11de485cf67d5ba23009
bonustrack <fabien@bonustrack.co>
2022-07-17 00:11:22 +0700

    fix: block.transaction_receipts is not iterable

--- src/checkpoint.ts
+++ src/checkpoint.ts
@@ -231,7 +231,7 @@ export default class Checkpoint {
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    for (const receipt of block.transaction_receipts) {
+    for (const receipt of block.transaction_receipts || []) {
       await this.handleTx(block, block.transactions[receipt.transaction_index], receipt);
     }
 
