4bc8c1f08e04c9049a490427b6ebcdc58988f1cc
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-12-14 13:34:06 +0100

    fix: replace getPendingTransactions with getBlockWithTxs (#275)
    getPendingTransactions was removed

--- src/providers/starknet/index.ts
+++ src/providers/starknet/index.ts
@@ -75,9 +75,9 @@ export class StarknetProvider extends BaseProvider {
   }
 
   async processPool(blockNumber: number) {
-    const txs = await this.provider.getPendingTransactions();
+    const block = await this.provider.getBlockWithTxs('pending');
     const receipts = await Promise.all(
-      txs.map(async tx => {
+      block.transactions.map(async tx => {
         if (!tx.transaction_hash) return null;
 
         try {
@@ -92,7 +92,7 @@ export class StarknetProvider extends BaseProvider {
       })
     );
 
-    const txsWithReceipts = txs.filter((_, index) => receipts[index] !== null);
+    const txsWithReceipts = block.transactions.filter((_, index) => receipts[index] !== null);
     const eventsMap = receipts.reduce((acc, receipt) => {
       if (receipt === null) return acc;
 
