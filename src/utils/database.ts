50bf9606a8cee1df6a92d66d336ee2c28daa2945
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-01-20 13:39:45 +0100

    refactor: remove legacy applyBlockFilter

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -13,14 +13,6 @@ export const getTableName = (name: string) => {
   return pluralize(name);
 };
 
-export function applyBlockFilter(query: Knex.QueryBuilder, tableName: string, block?: number) {
-  if (INTERNAL_TABLES.includes(tableName)) return query;
-
-  return block !== undefined
-    ? query.andWhereRaw(`${tableName}.block_range @> int8(??)`, [block])
-    : query.andWhereRaw(`upper_inf(${tableName}.block_range)`);
-}
-
 export function applyQueryFilter(
   query: Knex.QueryBuilder,
   tableName: string,
